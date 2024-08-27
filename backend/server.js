8f1fb0f5ca8f8324df00855cec8396fad1d53545
Raas-Farooq <raasfarooq@gmail.com>
2024-08-27 08:54:42 +0500

    add login and register routes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import databaseConnection from './config/db.js';
 import bcrypt from 'bcrypt';
 import {User} from './models/model.js';
-
+import rateLimit from 'express-rate-limit';
+import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 const app = express();
 
 app.use(express.json());
@@ -10,30 +12,60 @@ app.use(express.json());
 const port = process.env.PORT || 4100;
 databaseConnection()
 
-app.get('/weblog/registerUser', async (req,res) => {
-    console.log("My App is running ");
-    const {newUsername, newEmail, newPassword} = req.body;
+const registerLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max:5
+})
+
+
+app.post('/weblog/registerUser',registerLimiter,
+[
+    body('username').isLength({min:3}).trim().escape(),
+    body('email').isEmail().normalizeEmail(),
+    body('password').isLength({min:8})
+]
+, async (req,res) => {
+
+    const errors = validationResult(req);
+    console.log("validation Result to Err: ", errors);
+    if(!errors.isEmpty()){
+        return res.status(404).json({
+            success:false,
+            message:"Validation Error",
+            error:errors.array()
+        })
+    }
 
     try{
-        const protectedPassword = await bcrypt.hash(newPassword, 10);
-        
+            
+        console.log("webLog is Growing faster than Plant ");
+        const {username, email, password} = req.body;
+        console.log("webLog is Growing faster than Plant ", username, " ", email);
+        const isUserExist = await User.findOne({email});
+
+        if (isUserExist){
+            return res.status(404).json({
+                success:false,
+                message:"Username or email already Taken",
+            })
+        }
+        const saltRange = 10;
+        const protectedPassword = await bcrypt.hash(password, saltRange);
+        console.log("protected Password", protectedPassword);
         const newUser = new User({
-            username: newUsername,
-            email:newEmail,
+            username: username,
+            email:email,
             password:protectedPassword
         })
 
         
             await newUser.save();
 
-            if(!newUser){
-                res.status(404).json({
-                    success:false,
-                    message:`Something goes wrong`,
-                    err:err.message
-                })
-            }
-            
+           const newUserObject = newUser.toObject();
+           console.log("newUserObject", newUserObject);
+
+           delete newUserObject.password;
+
             res.status(201).json({
                 success:true,
                 message:"User Has been added"
@@ -42,35 +74,73 @@ app.get('/weblog/registerUser', async (req,res) => {
         
     }
     catch(err){
-        if(err.code === 11000){
-            const errElement = Object.keys(err.keyCode[0]);
-
-            res.status(404).json({
-                success:false,
-                message:`${errElement} has already defined, take any other element`,
-                err:err.message
-            })
-        }
-
-        if(err.name === 'ValidationError'){
-
-            res.status(404).json({
-                success:false,
-                message:"Validation Error",
-                error:err.message
-            })
-        }
-        else{
             res.status(500).json({
                 success:false,
                 message:`Error Occured while server processing`,
                 err:err.message
             })
-        }
     }
     
 })
 
+
+const loginLimiter = rateLimit({
+    windowMs:15 * 60 * 1000,
+    max:5
+})
+
+app.get('/weblog/userLogin',loginLimiter, [
+    body('email').isEmail().normalizeEmail(),
+    body('password').notEmpty()
+], async(req,res) => {
+
+    const loginErros = validationResult(req);
+    if(!loginErros.isEmpty()){
+        return res.status(404).json({
+            success:false,
+            message:"email or password not Valid",
+            errors:loginErros.array()
+        })
+    }
+    
+    try{
+        const {email, password}= req.body;
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(404).json({
+                success:false,
+                message:"user didn't exist"
+            })
+        }
+
+        const isPasswordMatched = await bcrypt.compare(user.password, password);
+        if(!isPasswordMatched){
+            res.status(404).json(
+                {
+                    success:false,
+                    message:"password didn't Match. Try again please"
+                }
+            )
+        }
+
+        jwt.sign(
+            ({userId:user._id, email:user.email}), 
+            process.env.JWT_SECRET,
+             {expiresIn: '1h'}
+            )
+        res.status(201).json({
+            success:true,
+            message:"Successfully Logged In"
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            success:false,
+            message:`server Error Occured while logging`,
+            err:err.message
+        })
+    }
+})
 app.listen((port), () => console.log("App is running On ", port));
 
 
