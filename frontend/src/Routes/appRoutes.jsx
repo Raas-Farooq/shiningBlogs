639b58ad2015dc6b78da7a5d032a5a3e740eda78
Raas-Farooq <raasfarooq@gmail.com>
2024-09-02 21:41:02 +0500

    add transition to links

--- frontend/src/Routes/appRoutes.jsx
+++ frontend/src/Routes/appRoutes.jsx
@@ -1,23 +1,44 @@
 // import BrowserRouter, {routes, Route} from 'react-router-dom';
 
-import { BrowserRouter, Route,Routes } from "react-router-dom"
+import { BrowserRouter, Route,Routes, useLocation } from "react-router-dom"
 import App from "../App"
 import About from "../pages/About"
 import Write from "../pages/write"
 import Content from "../pages/content"
+import { CSSTransition, Transition, TransitionGroup } from "react-transition-group"
 
 
 
-const AppRoutes = () => {
+const PageTransition = () => {
+    const location = useLocation();
 
     return (
-        <BrowserRouter>
-            <Routes>
+
+        <TransitionGroup className="page-container">
+            <CSSTransition 
+            
+            timeout={500}
+            key={location.key}
+            classNames="slide"
+            >
+            <Routes location={location}>
                 <Route path="/" element={<App />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/write" element={<Write />} />
                 <Route path="/content" element={<Content />} />
             </Routes>
+            </CSSTransition>
+        </TransitionGroup>
+            
+        
+    )
+}
+
+const AppRoutes = () => {
+
+    return (
+        <BrowserRouter>
+            <PageTransition />
         </BrowserRouter>
     )
 }
