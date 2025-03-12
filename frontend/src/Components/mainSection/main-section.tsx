e97f6932db274b4ed44289ae6e36b8df5b42bb1d
Raas-Farooq <raasfarooq@gmail.com>
2025-03-12 10:32:07 +0500

    add Wakeup backend

--- frontend/src/Components/mainSection/main-section.tsx
+++ frontend/src/Components/mainSection/main-section.tsx
@@ -1,11 +1,22 @@
 import clsx from "clsx";
 import { useBlogContext, useUIContext } from "../../globalContext/globalContext"
+import { useEffect } from "react";
+import { VITE_API_URL } from "../../config";
 
 
 const MainSection = () => {
 
     const {openUserAccount = false, showMenu = false, editProfile = false} = useUIContext() || {};
     const {searching=false} = useBlogContext() || {};
+
+    useEffect(() => {
+        console.log("VITE_APi url: inside Main-Section", VITE_API_URL);
+        fetch(`${VITE_API_URL}/wake-up`)
+  .catch(() => console.log("Backend might be sleeping, retrying..."));
+
+    }, [])
+
+
     return (
         <>
             <div className={clsx(`flex justify-center h-full`,(openUserAccount || showMenu || editProfile) && 'mt-12', (showMenu && searching) && 'hidden')}>
